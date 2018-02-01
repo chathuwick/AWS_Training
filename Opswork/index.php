26029a431e7de275899737d919a53cd11f516982
chathuwick <35885787+chathuwick@users.noreply.github.com>
2018-02-01 14:40:16 +0300

    Create index.php
    to test OpsWork

---
+++ Opswork/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Hello AWS</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>Hello AWS</h1>
+                <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
+                <div align="center"><img src="https://s3.amazonaws.com/chathu69/AWS.png"></div>
+                <p>You OpsWork Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
