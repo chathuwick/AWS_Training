908e55de663102bd36b95ad8059889c3853786e1
chathuwick <35885787+chathuwick@users.noreply.github.com>
2018-05-04 10:59:56 +0300

    Create connect.php

---
+++ RDS/connect.php
@@ -0,0 +1,14 @@
+<?php
+$username = "";
+$password = "";
+$hostname = ""; 
+$dbname = "";
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+  or die("Unable to connect to MySQL");
+echo "Connected to MySQL using username - $username, password - $password, host - $hostname<br>";
+$selected = mysql_select_db("$dbname",$dbhandle) 
+  or die("Could not select database");
+echo "Connected to Database - $dbname";
+?>
